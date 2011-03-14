be6962f4c81a35764ce9eb3e4065ed6ffc2522e1
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-03-14 14:32:47 +0000

    Wrapper ready to collect some real statistics.

--- curllog.py
+++ curllog.py
@@ -31,7 +31,7 @@ class DapRequest(object):
 
     @classmethod
     def from_get(klass, host, get_request):
-        mo = re.match(r'GET ([^?]*)\.([^?]+)(\?.*)? HTTP/[0-9.]+$', get_request)
+        mo = re.match(r'GET ([^?]*)\.([^?]+)(\?.*)? HTTP/[0-9.]+\s*$', get_request)
         if not mo:
             raise ValueError("GET request not recognised")
         path, response, query = mo.groups()
@@ -64,25 +64,77 @@ class DapStats(object):
     """
 
 
-    def __init__(self, requests=None):
+    def __init__(self, timed_requests=None):
         self.datasets = {}
 
-        self.update(requests)
+        self.last_timestamp = None
+        self.last_request = None
+        self.final = False
 
-    def add_req(self, request):
-        ds_stat = self.datasets.setdefault(request.dataset, {})
-        resp = ds_stat.response
-        if resp not in ['das', 'dds', 'dods']:
-            resp = '?'
-        rlist = ds_stat.setdefault(resp, [])
-        rlist.append(request)
+        self.update(timed_requests)
 
-        ds_stats[resp].append(request)
 
+    def add_req(self, timestamp, request):
+        if self.final:
+            raise ValueError("Request stream has been finalised")
+
+        if self.last_timestamp:
+            self._unstage_req(timestamp)
+
+        # Stage the current request
+        self.last_timestamp = timestamp
+        self.last_request = request
             
-    def update(self, requests):
-        for request in requests:
-            self.add_req(request)
+    def update(self, timed_requests):
+        for timestamp, request in timed_requests:
+            # If request is None it is the end timestamp.
+            if request is None:
+                self.final_event(timestamp)
+                self.final = True
+            else:
+                self.add_req(timestamp, request)
+
+    def final_event(self, timestamp):
+        # Add final request
+        self._unstage_req(timestamp)
+
+    def get_cursor(self, dataset=None):
+        if dataset is None:
+            dataset = self.datasets.keys()[0]
+
+        return DapStatsCursor(self, dataset)
+
+    def _unstage_req(self, timestamp):
+        # Add the last request
+        ds_stat = self.datasets.setdefault(self.last_request.dataset, [])
+        ds_stat.append((self.last_timestamp, timestamp, self.last_request))
+
+
+
+class DapStatsCursor(object):
+    """
+    Points to a specific dataset in DapStats.
+
+    """
+    def __init__(self, stats, dataset):
+        self.ds_stats = stats.datasets[dataset]
+
+    def response_count(self, response):
+        return len(list(self._iter_response(response)))
+
+    def response_tmean(self, response):
+        ts = (end - start for start, end, req 
+              in self._iter_response(response))
+        return mean(ts)
+
+    def response_hist(self, response, bins=10):
+        ts = (end - start for start, end, req
+              in self._iter_response(response))
+        return numpy.histogram(ts, bins=bins)
+
+    def _iter_response(self, response):
+        return (t for t in self.ds_stats
+                if t[2].response == response)
 
 
 def iter_requests(fh):
--- record_dap.py
+++ record_dap.py
@@ -9,9 +9,13 @@ import tempfile
 import os, sys
 from subprocess import Popen, PIPE
 import re
+import urllib
+
+from curllog import DapRequest, DapStats
 
 TMP_PREFIX='record_dap-'
 DODSRC = '.dodsrc'
+LOGFILE = 'record_dap.log'
 
 class Wrapper(object):
     def __init__(self, tmpdir=None):
@@ -25,19 +29,50 @@ class Wrapper(object):
         os.environ['HOME'] = self.tmpdir
 
     def write_dodsrc(self):
+        #!TODO: not suitable for secured opendap yet.
         fh = open(self.dodsrc)
         fh.write("""
 DEFLATE=0
 CURL.VERBOSE=1
 """)
 
-    def trace_exec(self, command):
+    def call(self, command):
         cmd = 'strace -ttt -f -e trace=network %s' % command
-        pipe = Popen(cmd, shell=True, stdout=PIPE).stdout
+        pipe = Popen(cmd, shell=True, stderr=PIPE, 
+                     stdout=open('/dev/null')).stderr
 
-        return self.filter_output(pipe)
+        return DapStats(self.iter_requests(pipe))
 
-    def filter_output(self, pipe):
+    def iter_requests(self, pipe):
+        timestamp = None
+        host = 'unknown'
         for line in pipe:
-            if re.match('^> GET|[\d\:\.]+\ssend\(\d+, "GET'):
-                yield line
+            mo = re.match('\* Connected to ([^\s]+)', line)
+            if mo:
+                host = mo.group(1)
+            elif re.match('> GET ', line):
+                #!TODO: handle other stderr output from wrapped tool
+                req = urllib.unquote(line.strip()[2:])
+                request = DapRequest.from_get(host, req)
+                assert timestamp is not None
+                yield (timestamp, request)
+                timestamp = None
+            else:                
+                mo = re.match('(\d+\.\d+)\s+(send|recv)', line)
+                if mo:
+                    timestamp, syscall = mo.groups()
+                    timestamp = float(timestamp)
+
+        # Mark terminal event
+        yield (timestamp, None)
+
+
+if __name__ == '__main__':
+    #test_dataset = 'http://esg-dev1.badc.rl.ac.uk:8081/ta_20101129/ta_6hrPlev_HadGEM2-ES_piControl_r1i1p1_197812010600-197901010000.nc'
+    w = Wrapper('.')
+    stats = w.call('cdo runmean,20 http://esg-dev1.badc.rl.ac.uk:8080/opendap/ta_20101129/ta_6hrPlev_HadGEM2-ES_piControl_r1i1p1_197812010600-197901010000.nc out.nc')
+    ds_stats = stats.get_cursor()
+
+    print ds_stats
+
+
