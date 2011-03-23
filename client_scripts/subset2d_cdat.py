7e5ad490bfb299540e0e226a49b5f6df040f5777
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-03-23 06:18:23 +0000

    Client scripts to wrap with record_dap.

---
+++ client_scripts/subset2d_cdat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# BSD Licence
+# Copyright (c) 2011, Science & Technology Facilities Council (STFC)
+# All rights reserved.
+#
+# See the LICENSE file in the source distribution of this software for
+# the full license text.
+
+# subset_2d_cdo.sh
+#
+#
+# Scripts implementing this test should subset a dataset to a given
+# bounding box.  Scripts can assume the given variable is a 2D field.
+#
+#  usage: %prog dataset variable bbox outputfile
+#
+# where bbox = 'lon0,lon1,lat0,lat1'
+
+import sys
+import cdms2
+
+dataset, variable, bbox, outfile = sys.argv[1:]
+
+lon0, lon1, lat0, lat1 = (float(x) for x in bbox.split(','))
+
+ds = cdms2.open(dataset)
+var = ds[variable]
+
+subset = var(longitute=(lon0, lon1),
+             latitude=(lat0, lat1))
+
+out_ds = cdms2.open(outfile, 'w')
+out_ds.write(subset)
+
+out_ds.close()
---
+++ client_scripts/subset2d_cdo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# subset_2d_cdo.sh
+#
+#
+# Scripts implementing this test should subset a dataset to a given
+# bounding box.  Scripts can assume the given variable is a 2D field.
+#
+#  usage: %prog dataset variable bbox outputfile
+#
+# where bbox = 'lon0,lon1,lat0,lat1'
+
+set -x
+
+TEMP1=subset_2d_tmp
+
+dataset=$1
+variable=$2
+bbox=$3
+outfile=$4
+
+# Extract bbox over all variables then split on variable
+cdo sellonlatbox,$bbox $dataset $TEMP1.nc
+cdo splitname $TEMP1.NC
+
+# Rename selected variable
+mv $TEMP1.$variable.nc $outfile
+rm $TEMP1.*.nc
