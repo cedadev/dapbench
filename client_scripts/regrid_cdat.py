bd1687f9a434e7dbb2328253f24d186a7c030701
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-04-04 22:22:06 +0100

    Implemented regrid script for cdat.

--- client_scripts/regrid_cdat.py
+++ client_scripts/regrid_cdat.py
@@ -11,10 +11,32 @@
 # where lons and lats are integers representing number of grid points
 
 import sys
+import cdms2
 
 dataset, lons, lats, outfile = sys.argv[1:]
 
 lons = int(lons)
 lats = int(lats)
 
-#!TODO
+#!TODO: should I check order='yx'
+#!TODO: refine grid spec
+dest_grid = cdms2.createUniformGrid(-90, lats, 180./lats, 
+                                    0, lons, 360./lons)
+
+
+ds = cdms2.open(dataset)
+# Select first variable with a grid
+for var in ds.variables.values():
+    if var.getGrid():
+        print 'Regridding variable %s' % var.id
+        break
+else:
+    raise Exception("No variable with grid found")
+
+new_var = var.regrid(dest_grid)
+
+out = cdms2.open(outfile, 'w')
+out.write(new_var, extend=0)
+
+out.close()
+ds.close()
