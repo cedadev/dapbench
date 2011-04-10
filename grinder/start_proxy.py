2bafa9b70896ca767c5974f9ec2f2cea15f3c179
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-04-10 21:51:07 +0100

    Patched grinder's default EchoFilter to add timestamps. start_proxy.sh automatically includes this filter.

--- grinder/start_proxy.py
+++ grinder/start_proxy.py
@@ -1,8 +1,22 @@
 #!/bin/sh
+#
+# Start grinder's TCPProxy in a mode suitable for capturing
+# OPeNDAP requests.  This script uses TimestampedEchoFilter to
+# add timestamps to TCPProxy's basic request filters.  See
+# grinder/src/TimestampedEchoFilter.
+#
 
 cd `dirname $0`
 LIBDIR=$PWD/lib
 CLASSPATH=`ls $LIBDIR/*.jar | tr '\n' ':'`
+
+# Add src directory for TimestampedEchoFilter
+CLASSPATH=$CLASSPATH:$PWD/src
+
 JAVA_OPTS=
 
-java -cp $CLASSPATH $JAVA_OPTS net.grinder.TCPProxy $@
+# Invoke with required filters and pass on any further arguments.
+java -cp $CLASSPATH $JAVA_OPTS net.grinder.TCPProxy \
+    -requestfilter TimestampedEchofilter \
+    -responsefilter net.grinder.tools.tcpproxy.NullFilter \
+    $@
