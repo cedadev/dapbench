196b5d64b7b369735647f79e8cb32d9ad35eafab
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2012-04-10 23:54:41 +0100

    Better consoleless agent configuration

--- grinder/start_agent.sh
+++ grinder/start_agent.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
+#
+# usage: ./start_agent.sh $GRINDERPROPERTIES $JAVA_OPT1 $JAVA_OPT2 ...
 cd `dirname $0`
 LIBDIR=$PWD/../lib
 CLASSPATH=`ls $LIBDIR/*.jar | tr '\n' ':'`
+
+GRINDERPROPERTIES=$1 ; shift
 JAVA_OPTS=$@
 
-java -cp $CLASSPATH $JAVA_OPTS net.grinder.Grinder
+java -cp $CLASSPATH $JAVA_OPTS net.grinder.Grinder $GRINDERPROPERTIES
